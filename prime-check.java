256d659706c7fd32166471b2672304afd300e05f
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-01-07 11:09:41 -0600

    new model

--- prime-check.java
+++ prime-check.java
@@ -15,3 +15,22 @@ public class PrimalityTester0 {
             System.out.println(number + " is not prime.");
     }
 }
+
+// Revised prime check that does not check for multiples of 2 for efficiency
+import java.util.*;
+
+public class PrimalityTester1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        long number = in.nextLong();
+        boolean prime = number == 2 || number % 2 != 0;
+        for(long i = 3; i < number && prime; i += 2 )
+            if(number % i  ==  0)
+                prime = false;
+        if(prime)
+            System.out.println(number + " is prime.");
+        else
+            System.out.println(number + " is not prime.");
+    }
+}
